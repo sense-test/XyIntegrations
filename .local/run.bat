6be3ab8afca823728b7599de2b2cdb460e0fa3e9
Luis Garcia <luis.garcia@xygeni.io>
2023-05-09 17:22:33 +0200

    co modificado 2

--- .local/run.bat
+++ .local/run.bat
@@ -5,7 +5,7 @@ set XY_SRC_DIR=.
 set XY_SCANNER_DIR=C:\LGV\src\XyIntegrations\test_cmd\scanner
 
 
-call %XY_SCANNER_DIR%\xygeni codetamper -n %XY_PRJ_NAME% -d %XY_SRC_DIR%
+call %XY_SCANNER_DIR%\xygeni codetamper -n %XY_PRJ_NAME% -d %XY_SRC_DIR% -u
 
 echo on
 rmdir /S /Q logs  

4b84bcf1bd5dfd310b3a6c90efd4232e51a5b11e
Luis Garcia <luis.garcia@xygeni.io>
2023-05-09 17:05:21 +0200

    added codeowners

--- .local/run.bat
+++ .local/run.bat
@@ -1,8 +1,11 @@
 echo off
+echo on
 set XY_PRJ_NAME=CMD-XyIntegrations-LOCAL-CMD
 set XY_SRC_DIR=.
 set XY_SCANNER_DIR=C:\LGV\src\XyIntegrations\test_cmd\scanner
 
 
-%XY_SCANNER_DIR%\xygeni codetamper -n %XY_PRJ_NAME% -d %XY_SRC_DIR%
+call %XY_SCANNER_DIR%\xygeni codetamper -n %XY_PRJ_NAME% -d %XY_SRC_DIR%
+
+echo on
 rmdir /S /Q logs  

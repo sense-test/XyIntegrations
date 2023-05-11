d5348ccbd65287a547eadfac26b3641d7e671082
Luis Garcia <luis.garcia@xygeni.io>
2023-05-11 09:47:04 +0200

    xygeni scan --never-fail

--- .local/run.bat
+++ .local/run.bat
@@ -5,7 +5,7 @@ set XY_SRC_DIR=C:\LGV\src\XyIntegrations\XyIntegrations\src
 set XY_SCANNER_DIR=C:\LGV\src\XyIntegrations\test_cmd\scanner
 
 
-call %XY_SCANNER_DIR%\xygeni.cmd scan --last -n %XY_PRJ_NAME% -d %XY_SRC_DIR% -e **/scanner/**
+call %XY_SCANNER_DIR%\xygeni.cmd codetamper --last -u -n %XY_PRJ_NAME% -d %XY_SRC_DIR% -e **/scanner/**
 
 echo on
 rmdir /S /Q logs  

b5574241c52079e0339252a385119ee2f0715e90
Luis Garcia <luis.garcia@xygeni.io>
2023-05-11 09:14:00 +0200

    co modificado 5

--- .local/run.bat
+++ .local/run.bat
@@ -1,11 +1,11 @@
 echo off
 echo on
 set XY_PRJ_NAME=CMD-XyIntegrations-LOCAL-CMD
-set XY_SRC_DIR=C:\LGV\src\XyIntegrations\XyIntegrations
+set XY_SRC_DIR=C:\LGV\src\XyIntegrations\XyIntegrations\src
 set XY_SCANNER_DIR=C:\LGV\src\XyIntegrations\test_cmd\scanner
 
 
-call %XY_SCANNER_DIR%\xygeni.cmd scan -n %XY_PRJ_NAME% -d %XY_SRC_DIR% -e **/scanner/**
+call %XY_SCANNER_DIR%\xygeni.cmd codetamper -u -n %XY_PRJ_NAME% -d %XY_SRC_DIR% -e **/scanner/**
 
 echo on
 rmdir /S /Q logs  
